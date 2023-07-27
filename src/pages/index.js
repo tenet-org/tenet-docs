12ed08acbb8f74428955fabb699416d9d26e0647
Daniil Lashin <danil-lashin@yandex.ru>
2023-07-27 10:33:54 +0300

    Add 'Explore node docs' button to homepage
    A 'Explore node docs' button has been added to the homepage header. This change was made to improve website navigation and help users to easily access the Node.js documentation.

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,16 +2,24 @@ import React from 'react';
 import clsx from 'clsx';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
 
 import styles from './index.module.css';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx('hero', styles.heroBanner)}>
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+          <Link
+              className="button button--secondary button--lg"
+              to="/node/tutorials/running-node">
+              Explore node docs
+          </Link>
+        </div>
       </div>
     </header>
   );
