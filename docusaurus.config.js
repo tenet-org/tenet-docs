1443d6813bf3216b0726fe47ed08b1760e3a909b
Daniil Lashin <danil-lashin@yandex.ru>
2023-11-02 20:24:22 +0300

    Update production URL in Docusaurus config
    The commit includes the change of production URL in 'docusaurus.config.js'. The URL has been updated from 'https://tenet-org.github.io/' to 'https://docs.tenet.org/', adhering to our latest domain changes.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -40,7 +40,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://tenet-org.github.io/',
+  url: 'https://docs.tenet.org/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
